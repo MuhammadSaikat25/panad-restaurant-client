deed3c266312bb83eec2fae93651b8b706abff5d
MuhammadSaikat25 <smsaikat000@gmail.com>
2023-07-16 08:04:21 +0600

    added active nav

--- src/Shared/NavBar/Nav.jsx
+++ src/Shared/NavBar/Nav.jsx
@@ -10,12 +10,12 @@ const Nav = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu  menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 gap-3">
-                            <li><NavLink>Home</NavLink></li>
-                            <li><NavLink>Contact Us</NavLink></li>
-                            <li><NavLink>Menu</NavLink></li>
-                            <li><NavLink>Shop</NavLink></li>
-                            <li><NavLink>0</NavLink></li>
-                            <li><NavLink>Profile</NavLink></li>
+                            <li><NavLink className={({isActive})=>isActive?'text-orange-600':''}>Home</NavLink></li>
+                            <li><NavLink className={({isActive})=>isActive?'text-orange-600':''}>Contact Us</NavLink></li>
+                            <li><NavLink className={({isActive})=>isActive?'text-orange-600':''} to={'/menu'}>Menu</NavLink></li>
+                            <li><NavLink className={({isActive})=>isActive?'text-orange-600':''}>Shop</NavLink></li>
+                            <li><NavLink className={({isActive})=>isActive?'text-orange-600':''}>0</NavLink></li>
+                            <li><NavLink className={({isActive})=>isActive?'text-orange-600':''}>Profile</NavLink></li>
 
                         </ul>
                     </div>
@@ -23,12 +23,12 @@ const Nav = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex mr-9">
                     <div className=" flex gap-4">
-                        <NavLink>Home</NavLink>
-                        <NavLink>Contact Us</NavLink>
-                        <NavLink to={'/menu'}>Menu</NavLink>
-                        <NavLink>Shop</NavLink>
-                        <NavLink>0</NavLink>
-                        <NavLink>Profile</NavLink>
+                        <NavLink className={({isActive})=>isActive?'text-yellow-300':''}>Home</NavLink>
+                        <NavLink className={({isActive})=>isActive?'text-yellow-300':''}>Contact Us</NavLink>
+                        <NavLink className={({isActive})=>isActive?'text-yellow-300':''} to={'/menu'}>Menu</NavLink>
+                        <NavLink className={({isActive})=>isActive?'text-yellow-300':''}>Shop</NavLink>
+                        <NavLink className={({isActive})=>isActive?'text-yellow-300':''}>0</NavLink>
+                        <NavLink className={({isActive})=>isActive?'text-yellow-300':''}>Profile</NavLink>
                     </div>
                 </div>
             </div>
